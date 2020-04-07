40f31a585b62c9875c6ab76d82eba26a1bf4c1ae
zhenyatos <zhenyatos999@gmail.com>
2020-04-07 18:29:19 +0300

    Add createVector

--- Vector/IVector.cpp
+++ Vector/IVector.cpp
@@ -1,8 +1,39 @@
 #include "IVector.h"
+#include "VectorImpl.cpp"
 
 IVector* IVector::createVector(size_t dim, double* pData, ILogger* pLogger)
 {
-	return nullptr;
+	if (pData == nullptr)
+	{
+		pLogger->log("In [createVector] pData is nullptr", RESULT_CODE::WRONG_ARGUMENT);
+		return nullptr;
+	}
+
+	if (dim == 0)
+	{
+		pLogger->log("In [createVector] dim = 0", RESULT_CODE::WRONG_DIM);
+		return nullptr;
+	}
+
+	double* data = new (std::nothrow)double[dim];
+	if (data == nullptr)
+	{
+		pLogger->log("In [createVector] not enough memory for [double* data]", RESULT_CODE::OUT_OF_MEMORY);
+		return nullptr;
+	}
+
+	for (int i = 0; i < dim; i++)
+		data[i] = pData[i];
+
+	IVector* res = new (std::nothrow)VectorImpl(dim, data);
+	if (res == nullptr)
+	{
+		pLogger->log("In [createVector] not enough memory for [IVector* res]", RESULT_CODE::OUT_OF_MEMORY);
+		return nullptr;
+	}
+
+	pLogger->createLogger(res);
+	return res;
 }
 
 IVector* IVector::add(IVector const* pOperand1, IVector const* pOperand2, ILogger* pLogger)
