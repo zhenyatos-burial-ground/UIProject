2591b821a70ab48463de89e27726e9ed1d968fb2
zhenyatos <zhenyatos999@gmail.com>
2020-04-07 19:40:16 +0300

    Add IVector tests in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,54 @@
 ﻿#include <iostream>
-#include "Integer.h"
-#include "Rational.h"
-#include "Real.h"
+#include "IVector.h"
 
 using namespace std;
 
+ostream& operator<<(ostream& stream, const IVector* vector)
+{
+	if (vector == nullptr)
+		return stream;
+
+	size_t dim = vector->getDim();
+	stream << "[";
+	for (int i = 0; i < dim - 1; i++)
+		stream << vector->getCoord(i) << " ";
+	stream << vector->getCoord(dim - 1);
+	stream << "]";
+
+	return stream;
+}
+
 int main()
 {
-	Rational a1 = { 1, 3 };
-	Rational a2 = { 1, 2 };
-	
-	Rational b1 = { -1, 2 };
-	Rational b2 = { -1, 3 };
+	ILogger* logger = ILogger::createLogger(nullptr);
 
-	Real a = { a1, a2 };
-	Real b = { b1, b2 };
+	double data1[3] = { 0, 1, -1 };
+	IVector* vector1 = IVector::createVector(3, data1, logger);
 
-	Real test1 = a / b;
-	Real test2 = a + b;
-	b = Rational(-1, 2) * b;
+	double data2[3] = { 1, 0, 2 };
+	IVector* vector2 = IVector::createVector(3, data2, logger);
 
+	IVector* testAdd = IVector::add(vector1, vector2, logger);
+	std::cout << testAdd << "\n";
+
+	double testMul = IVector::mul(vector1, vector2, logger);
+	std::cout << testMul << "\n";
+
+	IVector* testSub = IVector::sub(vector1, vector2, logger);
+	std::cout << testSub << "\n";
+
+	IVector* testScale = IVector::mul(vector1, 2.5, logger);
+	std::cout << testScale << "\n";
+
+	IVector* testLog = IVector::add(nullptr, vector2, logger);
+
+	int x;
+	std::cin >> x;
+
+	delete vector1;
+	delete vector2;
+	delete testAdd;
+	delete testSub;
+	delete testScale;
 	return 0;
 }
