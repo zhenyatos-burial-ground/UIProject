891026f583c935248a5323a18b41317d584a8659
zhenyatos <zhenyatos999@gmail.com>
2020-05-30 23:23:07 +0300

    Add ISet::sub and ISet::symSub
    -Refactor validating data in operators

--- src/Numeric/Set/ISet.cpp
+++ src/Numeric/Set/ISet.cpp
@@ -2,39 +2,40 @@
 #include "SetImpl.cpp"
 #include <string>
 
-RESULT_CODE validateData(ISet const* pOperand1, ISet const* pOperand2, double tolerance, std::string fun, std::string& msg)
+RESULT_CODE validateData(ISet const* pOperand1, ISet const* pOperand2, double tolerance, std::string fun, ILogger* logger)
 {
+	RESULT_CODE code = RESULT_CODE::SUCCESS;
+	std::string msg;
+
 	if (pOperand1 == nullptr)
 	{
 		msg = "In " + fun + " pOperand1 is nullptr";
-		return RESULT_CODE::WRONG_ARGUMENT;
+		code = RESULT_CODE::WRONG_ARGUMENT;
 	}
-
-	if (pOperand2 == nullptr)
+	else if (pOperand2 == nullptr)
 	{
 		msg = "In " + fun + " pOperand2 is nullptr";
-		return RESULT_CODE::WRONG_ARGUMENT;
+		code = RESULT_CODE::WRONG_ARGUMENT;
 	}
-
-	if (pOperand1->getDim() == 0)
+	else if (pOperand1->getDim() == 0)
 	{
 		msg = "In " + fun + " pOperand1 dim = 0";
-		return RESULT_CODE::WRONG_DIM;
+		code = RESULT_CODE::WRONG_DIM;
 	}
-
-	if (pOperand2->getDim() == 0)
+	else if (pOperand2->getDim() == 0)
 	{
 		msg = "In " + fun + " pOperand2 dim = 0";
-		return RESULT_CODE::WRONG_DIM;
+		code = RESULT_CODE::WRONG_DIM;
 	}
-
-	if (pOperand1->getDim() != pOperand2->getDim())
+	else if (pOperand1->getDim() != pOperand2->getDim())
 	{
 		msg = "In " + fun + " pOperand1 dim is not equal pOperand2 dim";
-		return RESULT_CODE::WRONG_DIM;
+		code = RESULT_CODE::WRONG_DIM;
 	}
 
-	return RESULT_CODE::SUCCESS;
+	if (logger != nullptr)
+		logger->log(msg.c_str(), code);
+	return code;
 }
 
 ISet* ISet::createSet(ILogger* pLogger)
@@ -49,14 +50,9 @@ ISet* ISet::createSet(ILogger* pLogger)
 
 ISet* ISet::add(ISet const* pOperand1, ISet const* pOperand2, IVector::NORM norm, double tolerance, ILogger* pLogger)
 {
-	std::string msg;
-	RESULT_CODE code = validateData(pOperand1, pOperand2, tolerance, "[ISet::add]", msg);
+	RESULT_CODE code = validateData(pOperand1, pOperand2, tolerance, "[ISet::add]", pLogger);
 	if (code != RESULT_CODE::SUCCESS)
-		if (pLogger != nullptr)
-		{
-			pLogger->log(msg.c_str(), code);
-			return nullptr;
-		}
+		return nullptr;
 
 	ISet* res = pOperand1->clone();
 	if (res == nullptr)
@@ -78,14 +74,9 @@ ISet* ISet::add(ISet const* pOperand1, ISet const* pOperand2, IVector::NORM norm
 
 ISet* ISet::intersect(ISet const* pOperand1, ISet const* pOperand2, IVector::NORM norm, double tolerance, ILogger* pLogger)
 {
-	std::string msg;
-	RESULT_CODE code = validateData(pOperand1, pOperand2, tolerance, "[ISet::add]", msg);
+	RESULT_CODE code = validateData(pOperand1, pOperand2, tolerance, "[ISet::intersect]", pLogger);
 	if (code != RESULT_CODE::SUCCESS)
-		if (pLogger != nullptr)
-		{
-			pLogger->log(msg.c_str(), code);
-			return nullptr;
-		}
+		return nullptr;
 
 	ISet* res = ISet::createSet(pLogger);
 	if (res == nullptr)
@@ -106,6 +97,63 @@ ISet* ISet::intersect(ISet const* pOperand1, ISet const* pOperand2, IVector::NOR
 			delete vec2;
 		}
 		delete vec1;
+	}
+
+	return res;
+}
+
+ISet* ISet::sub(ISet const* pOperand1, ISet const* pOperand2, IVector::NORM norm, double tolerance, ILogger* pLogger)
+{
+	RESULT_CODE code = validateData(pOperand1, pOperand2, tolerance, "[ISet::sub]", pLogger);
+	if (code != RESULT_CODE::SUCCESS)
+		return nullptr;
+		
+
+	ISet* res = pOperand1->clone();
+	if (res == nullptr)
+		if (pLogger != nullptr)
+			pLogger->log("In [ISet:sub] not enough memory for [ISet* res]", RESULT_CODE::OUT_OF_MEMORY);
+
+	size_t size = pOperand2->getSize();
+	for (size_t indx = 0; indx < size; indx++)
+	{
+		IVector* vec = nullptr;
+		code = pOperand2->get(vec, indx);
+		if (code == RESULT_CODE::SUCCESS)
+			res->erase(vec, norm, tolerance);
+		delete vec;
+	}
+
+	return res;
+}
+
+ISet* ISet::symSub(ISet const* pOperand1, ISet const* pOperand2, IVector::NORM norm, double tolerance, ILogger* pLogger)
+{
+	RESULT_CODE code = validateData(pOperand1, pOperand2, tolerance, "[ISet::symSub]", pLogger);
+	if (code != RESULT_CODE::SUCCESS)
+		return nullptr;
+
+	ISet* add = ISet::add(pOperand1, pOperand2, norm, tolerance, pLogger);
+	if (add == nullptr)
+	{
+		if (pLogger != nullptr)
+			pLogger->log("In [ISet::symSub] not enough memory for [ISet* add]", RESULT_CODE::OUT_OF_MEMORY);
+		return nullptr;
+	}
+
+	ISet* intersect = ISet::intersect(pOperand1, pOperand2, norm, tolerance, pLogger);
+	if (intersect == nullptr)
+	{
+		if (pLogger != nullptr)
+			pLogger->log("In [ISet::symSub] not enough memory for [ISet* add]", RESULT_CODE::OUT_OF_MEMORY);
+		return nullptr;
+	}
+
+	ISet* res = ISet::sub(add, intersect, norm, tolerance, pLogger);
+	if (res == nullptr)
+	{
+		if (pLogger != nullptr)
+			pLogger->log("In [ISet::symSub] not enough memory for [ISet* res]", RESULT_CODE::OUT_OF_MEMORY);
 	}
 
 	return res;
